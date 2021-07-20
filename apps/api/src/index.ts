da6eec9c3a3ae4a66b2afbe091cacb458e6e0c5b
raloliver <contato@raloliver.com.br>
2021-07-20 11:07:25 +0100

    303: setup GraphQL Yoga

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,8 +2,17 @@
  * File: index.ts
  * Project: api
  * Created: Tuesday, July 20th 2021, 10:33:59 am
- * Last Modified: Tuesday, July 20th 2021, 10:35:32 am
+ * Last Modified: Tuesday, July 20th 2021, 11:06:37 am
  * Copyright © 2021 AMDE Agência
  */
 
-console.log("Hello GraphQL!");
+import server from "./server";
+
+const { PORT: port = 4000 } = process.env;
+
+const main = async (): Promise<void> => {
+  await server.start({ port });
+  console.log(`Listing on port ${port}...`);
+};
+
+main().catch(console.log);
---
+++ apps/api/src/server.ts
@@ -0,0 +1,13 @@
+/*
+ * File: server.ts
+ * Project: api
+ * Created: Tuesday, July 20th 2021, 11:00:53 am
+ * Last Modified: Tuesday, July 20th 2021, 11:02:15 am
+ * Copyright © 2021 AMDE Agência
+ */
+
+import { GraphQLServer } from "graphql-yoga";
+
+const server = new GraphQLServer({});
+
+export default server;
