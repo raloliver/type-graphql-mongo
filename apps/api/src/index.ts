03546fdb9e9180d0bccfa7f7e3d4cee1202ea4a6
raloliver <contato@raloliver.com.br>
2021-07-20 10:35:48 +0100

    301: configure nodemon and sucrase

---
+++ apps/api/src/index.ts
@@ -0,0 +1,9 @@
+/*
+ * File: index.ts
+ * Project: api
+ * Created: Tuesday, July 20th 2021, 10:33:59 am
+ * Last Modified: Tuesday, July 20th 2021, 10:35:32 am
+ * Copyright © 2021 AMDE Agência
+ */
+
+console.log("Hello GraphQL!");
